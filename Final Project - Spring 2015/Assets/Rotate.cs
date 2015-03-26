c9a662c519d5c84ff198fef04467a6f3a1d5477f
Keiji <kimuk170@newschool.edu>
2015-03-25 21:17:25 -0400

    no message

---
+++ Final Project - Spring 2015/Assets/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	void Update() {
+		transform.Rotate(Vector3.right * Time.deltaTime * 10f);
+		transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
